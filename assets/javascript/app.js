06cd3e7f267e116e3d83aa0ddcda69d6297e7f7e
Romy Owens <rdoed160@gmail.com>
2019-10-10 22:58:32 -0400

    initialize Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
-/* global moment firebase */
-
 // Initialize Firebase
 // Make sure to match the configuration to the script version number in the HTML
 // (Ex. 3.0 != 3.7.0)
+
+
 var firebaseConfig = {
-    apiKey: "AIzaSyAb68qaJaazY06LgNfGwgu6eMmLpauARZg",
-    authDomain: "employee-spreadsheet-2c83c.firebaseapp.com",
-    databaseURL: "https://employee-spreadsheet-2c83c.firebaseio.com",
-    projectId: "employee-spreadsheet-2c83c",
-    storageBucket: "",
-    messagingSenderId: "926542516001",
-    appId: "1:926542516001:web:3a513301adbff81607b860"
+    apiKey: "AIzaSyCWzt1G-BgKjEAjYPH5Om7vVrls9R-iQ4g",
+    authDomain: "train-times-589ee.firebaseapp.com",
+    databaseURL: "https://train-times-589ee.firebaseio.com",
+    projectId: "train-times-589ee",
+    storageBucket: "train-times-589ee.appspot.com",
+    messagingSenderId: "295453346198",
+    appId: "1:295453346198:web:0062abad3bc0a2b61ff5b7"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
